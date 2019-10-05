370f02fe8cfd9a1d271c0aca9f78dc388bf29d1b
Konstantin <konstantin.saigushev.com>
2019-10-05 16:14:38 +0600

    add render form method

--- src/App.php
+++ src/App.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App;
 
 use Psr\Http\Message\ResponseInterface;
+use FormManager\Factory as Form;
 
 class App
 {
@@ -18,9 +19,22 @@ class App
 
     public function __invoke(): ResponseInterface
     {
+        $form = $this->renderForm();
         $response = $this->response->withHeader('Content-Type', 'text/html');
-        $response->getBody()->write("<html><head></head><body>Hello, {$this->foo} world!</body></html>");
-        
+        $response->getBody()->write("<html><head></head><body>{$form}</body></html>");
+
         return $response;
+    }
+
+    public function renderForm()
+    {
+        $form = Form::form([
+            'name' => Form::text('Имя', ['name' => 'name', 'placeholder' => 'Имя']),
+            'email' => Form::email('Email', ['name' => 'email', 'type' => 'email', 'placeholder' => 'E-mail']),
+            'password' => Form::password('Password', ['name' => 'password', 'type' => 'password', 'placeholder' => 'Пароль']),
+            'submit' => Form::submit('Зарегистрироваться', ['name' => 'register', 'value' => "Зарегистрироваться"])
+        ], ['method' => 'POST', 'action' => '/register']);
+
+        return $form;
     }
 }
